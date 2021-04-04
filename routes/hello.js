a7b65b1d0c054a482ba0249a1f8edfbf88947d60
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-04 14:40:39 +0900

    外部サイトに接続するまでコミット

--- routes/hello.js
+++ routes/hello.js
@@ -1,39 +1,39 @@
 const express = require('express');
 const router = express.Router();
+// xml2jsモジュールを取り込む。
+const parseString = require('xml2js').parseString;
+const http = require('http');
 
 /* 
  * GETアクセス時の処理 
  */
 router.get('/', (req, res, next) => {
-    // メッセージ用の変数を用意する。
-    var msg = '※何か書いて送信してください。';
-
-    if (req.session.message != undefined) {
-        msg = "Last Message：" + req.session.message;
-    }
-
-    var data = {
-        title: 'Hello!',
-        content: msg
+    // インターネット上の情報を取得する変数
+    var opt = {
+        host: 'news.google.com',
+        port: 443,
+        path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja'
     };
-    // hello.ejsを呼び出す。
-    res.render('hello', data);
-});
-
-/* 
- * POSTアクセス時の処理 
- */
-router.post('/post', (req, res, next) => {
-    // message要素から値を取り出す。
-    var msg = req.body['message'];
-    // セッションから値を取得する。
-    req.session.message = msg;
-    var data = {
-        title: 'Hello!',
-        content: "Last Message：" + req.session.message 
-    };
-    // hello.ejsを呼び出す。(dataをセットする。)
-    res.render('hello', data);
+    // RSSから必要な情報を取り出す。
+    http.get(opt, (res2) => {
+        var body = '';
+        res2.on('data', (data) => {
+            body += data;
+        });
+        // データを受信し終わった時の処理
+        res2.on('end', () => {
+            parseString(body.trim(), (err, result) => {
+                // 取得結果を表示する。
+                console.log(result);
+                var data = {
+                    title: 'Google News',
+                    content: result.rss.channel[0].item
+                };
+                // hello.ejsを呼び出す。
+                res.render('hello', data);
+            });
+        })
+    });
 });
 
 module.exports = router;
