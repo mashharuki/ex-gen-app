e403b85151ae5ec3fffb4d848eb1788232140850
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-04 12:37:31 +0900

    POSTアクセス時の処理を追加した。

--- routes/hello.js
+++ routes/hello.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* 
+ * GETアクセス時の処理 
+ */
 router.get('/', (req, res, next) => {
     var data = {
         title: 'Hello!',
-        content: 'これは、サンプルのコンテンツです。<br>this is sample content.'
+        content: '何か書いて送信してください。'
     };
     // hello.ejsを呼び出す。
+    res.render('hello', data);
+});
+
+/* 
+ * POSTアクセス時の処理 
+ */
+router.post('/post', (req, res, next) => {
+    // message要素から値を取り出す。
+    var msg = req.body['message'];
+    var data = {
+        title: 'Hello!',
+        content: 'あなたは、「' + msg + '」と送信しました。'
+    };
+    // hello.ejsを呼び出す。(dataをセットする。)
     res.render('hello', data);
 });
 
