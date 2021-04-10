7503fe354b27449077c1fadf74bc4f1820098712
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-10 16:37:05 +0900

    レコードを追加する画面を追加する。

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Forbidden } = require('http-errors');
 const router = express.Router();
 // DBモジュールを読み込む
 const db = require ('../models/index'); 
@@ -15,6 +16,63 @@ router.get('/', (req, res, next) => {
     }
     res.render('users/index', data);
   });
+});
+
+/* 
+ * add画面 GETアクセス時の処理 
+ */
+router.get('/add', (req, res, next) => {
+  var data = {
+      title: 'Users/Add'
+  }
+  res.render('users/add', data);
+});
+
+/* 
+* add画面 POSTアクセス時の処理 
+*/
+router.post('/add', (req, res, next) => {
+    // SQLを実行する。
+    db.sequelize.sync().then( () => db.User.create({
+      name: req.body.name,
+      pass: req.body.pass,
+      mail: req.body.mail,
+      age: req.body.age
+    })).then(users => {
+      // usersのインデックス画面に遷移する。
+      res.redirect('/users');
+    });
+});
+
+/* 
+ * edit画面 GETアクセス時の処理 
+ */
+router.get('/edit', (req, res, next) => {
+  db.User.findByPk(req.query.id).then(user => {
+    var data = {
+      title: 'Users/Edit',
+      form: user
+    }
+    res.render('users/edit', data);
+  });
+});
+
+/* 
+* edit画面 POSTアクセス時の処理 
+*/
+router.post('/edit', (req, res, next) => {
+    // SQLを実行する。
+    db.sequelize.sync().then( () => db.User.update({
+      name: req.body.name,
+      pass: req.body.pass,
+      mail: req.body.mail,
+      age: req.body.age
+    },{
+      where: { id: req.body.id }
+    })).then(users => {
+      // usersのインデックス画面に遷移する。
+      res.redirect('/users');
+    });
 });
 
 module.exports = router;
